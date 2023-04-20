408e6b6a86cef3b53e49b5da5a9151e54b082c9d
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2023-04-20 12:04:37 +1100

    Stage 2

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from random import choice
+
+
 options = ('rock', 'paper', 'scissors')
-print('Sorry, but the computer chose %s' % options[(options.index(input()) + 1) % len(options)])
+go_options = ('There is a draw ({})',
+              'Well done. The computer chose {} and failed',
+              'Sorry, but the computer chose {}')
+user, comp = options.index(input()), choice(range(len(options)))
+print(go_options[user - comp].format(options[comp]))
