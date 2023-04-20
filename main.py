198c512058cba0c18aca70a7fbe23685c13fbbd1
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2023-04-20 12:05:01 +1100

    Stage 3

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from random import choice
 
 
-options = ('rock', 'paper', 'scissors')
-go_options = ('There is a draw ({})',
-              'Well done. The computer chose {} and failed',
-              'Sorry, but the computer chose {}')
-user, comp = options.index(input()), choice(range(len(options)))
-print(go_options[user - comp].format(options[comp]))
+class WrongEntry(Exception):
+    def __init__(self):
+        self.message = 'Invalid input'
+        super().__init__(self.message)
+
+
+class Game:
+    OPTIONS = ('rock', 'paper', 'scissors')
+    QUIT_CMD, QUIT_MSG = '!exit', 'Bye!'
+
+    def run(self):
+        while True:
+            try:
+                user, comp = input(), choice(self.OPTIONS)
+                if user == self.QUIT_CMD: break
+                if user not in self.OPTIONS: raise WrongEntry
+                print(Turn(user, comp))
+            except WrongEntry as err:
+                print(err)
+        print(self.QUIT_MSG)
+
+
+class Turn(Game):
+    msg_res, msg_drw = r'{pref} {t}he computer chose {comp} {post}', r'There is a draw ({comp})'
+    pref, t, post = ('Well done.', 'Sorry, but'), 'Tt', ('and failed', '')
+
+    def __init__(self, user, comp):
+        self.user, self.comp = user, comp
+        self.res = (self.OPTIONS.index(user) - self.OPTIONS.index(comp)) % len(self.OPTIONS) - 1
+
+    def __str__(self):
+        pref, t, post = map(lambda l: l[self.res], (self.pref, self.t, self.post))
+        return (*[self.msg_res] * 2, self.msg_drw)[self.res].format(pref=pref, t=t, post=post, comp=self.comp)
+
+
+my_game = Game()
+my_game.run()
