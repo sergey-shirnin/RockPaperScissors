ef93a46ee77ee1011acf8fa95df8aa16591d00c4
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2023-05-30 13:35:53 +1100

    Stage 5 (with create/read/write)

--- main.py
+++ main.py
@@ -13,30 +13,45 @@ class Game:
                    'water', 'air', 'paper', 'sponge', 'wolf',
                    'tree', 'human', 'snake', 'scissors', 'fire')
     DRAW_INC = 50  # WIN INC be twice as much
-    QUIT_CMD, RATING_CMD, QUIT_MSG, = '!exit', '!rating', 'Bye!'
+    QUIT_CMD, RATING_CMD, SAVE_CMD, QUIT_MSG = '!exit', '!rating', '!save', 'Bye!'
 
     def __init__(self, scores_file):
-        with open(scores_file) as f:
-            self.ratings = dict(line.split() for line in f)
-
+        self.filename = scores_file
+        self.ratings = self.read_db()
         self.player = Player(input('Enter your name: '), self.ratings)
+        self.commands = [self.QUIT_CMD, self.RATING_CMD, self.SAVE_CMD]
         self.options = [entry for entry in input().split(',') if entry] or self.OPTIONS_DEFAULT
+
+    def read_db(self, saved=False):
+        with open(self.filename, 'a+') as f:
+            if f.tell():
+                saved = True; f.seek(0)
+            return dict(map(lambda s: s.split(), f)) if saved else {}
+
+    def write_db(self):
+        with open(self.filename, 'w') as f:
+            for player in self.ratings.items():
+                f.write(' '.join(player + ('\n',)))
 
     def run(self):
         while True:
             print('{}Okay, let\'s start'.format('\n'[:self.options != self.OPTIONS_DEFAULT]))
             done = False
             while True:
+                print(self.ratings)
                 try:
                     self.player.entry, comp = input(), choice(self.options)
                     if self.player.entry == self.QUIT_CMD: done = True; break
                     if self.player.entry == self.RATING_CMD: print(self.player.get_score()); continue
-                    if self.player.entry not in [self.QUIT_CMD, self.RATING_CMD] + self.options:
-                        raise WrongEntry([self.QUIT_CMD, self.RATING_CMD], self.options);
+                    if self.player.entry == self.SAVE_CMD: self.write_db(); continue
+                    if self.player.entry not in self.commands + self.options:
+                        raise WrongEntry(self.commands, self.options)
                     print(Turn(self.player, comp))
+                    self.ratings[self.player.name] = str(self.player.score)
                 except WrongEntry as err:
                     print(err); break
             if done: break
+        self.write_db()
         print(self.QUIT_MSG)
 
 
@@ -44,13 +59,13 @@ class Player:
     def __init__(self, name, ratings):
         self.name, self.entry = name, None
         self.score = int(ratings.get(name, 0))
-        print('Hello, %s' % self.name)
+        print('{msg}, {name}'.format(msg='Welcome back' if self.name in ratings else 'Hello', name=self.name))
 
     def get_score(self):
         return 'Your rating: %s' % self.score
 
 
-class Turn(Game):
+class Turn:
     msg_res, msg_drw = r'{} {t}he computer chose {comp} {}', r'There is a draw ({comp})'
     pref, t, post = ('Well done.', 'Sorry, but'), 'Tt', ('and failed', '')
 
@@ -60,7 +75,7 @@ class Turn(Game):
         options = Game.OPTIONS_ALL[n:] + Game.OPTIONS_ALL[:n]
 
         is_draw, is_win = options.index(self.comp) == options.index(player.entry), \
-                          options.index(player.entry) > options.index(self.comp)
+            options.index(player.entry) > options.index(self.comp)
         self.res = [[3, 2], [1]][is_draw][is_win]
 
         player.score += self.res % 3 * Game.DRAW_INC; self.res -= 2
